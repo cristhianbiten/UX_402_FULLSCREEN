2817780b80c03a4e5e2fb0cff95273ac7ff59ae6
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2024-11-19 22:32:42 +0000

    add create function

--- webapp/controller/Flights.controller.js
+++ webapp/controller/Flights.controller.js
@@ -3,9 +3,10 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "student10/com/sap/training/ux402/controls/HoverButton",
     "sap/m/MessageToast",
-    "student10/com/sap/training/ux402/controls/PlaneInfo"
+    "student10/com/sap/training/ux402/controls/PlaneInfo",
+    "sap/m/MessageBox",
   ],
-  function (Controller, HoverButton, MessageToast, PlaneInfo) {
+  function (Controller, HoverButton, MessageToast, PlaneInfo, MessageBox) {
     "use strict";
 
     return Controller.extend(
@@ -41,6 +42,51 @@ sap.ui.define(
           });
         },
 
+        onHoverPress: function (oEvent) {
+          var carrid = oEvent.getSource().data("id");
+          var fldate = oEvent.getSource().data("date");
+          var connid = oEvent.getSource().data("connid");
+
+          MessageBox.confirm(
+            "Are you sure want to book this flight?",
+            function (oAction) {
+              if (sap.m.MessageBox.Action.OK === oAction) {
+                var oEntry = {};
+                oEntry.Carrid = carrid;
+                oEntry.Connid = connid;
+                oEntry.Fldate = fldate;
+                oEntry.Counter = "1";
+                oEntry.Bookid = "43535432";
+                oEntry.Customid = "00003406";
+                oEntry.Passname = "John Doe";
+
+                var oModel = this.getOwnerComponent().getModel();
+                var customerHeader = { "Content-Type": "application/json" };
+                oModel.setHeaders(customerHeader);
+
+                var that = this;
+                Promise.all([this._createBookingEntry(oModel, oEntry)]).then(
+                  that._handleBookingSuccess,
+                  that._handleBookingError
+                );
+              }
+            }.bind(this)
+          );
+        },
+
+        _createBookingEntry: function (oModel, oEntry) {
+          return new Promise(function (resolve, reject) {
+            oModel.create("/UX_C_Booking_TP", oEntry, {
+              success: function (oData, response) {
+                resolve(oData.Bookid, response);
+              },
+              error: function (oError) {
+                reject(oError);
+              },
+            });
+          });
+        },
+
         _onObjectMatched: function (oEvent) {
           var oArgs = oEvent.getParameter("arguments");
           this._sCarrierId = oArgs.carrid;
@@ -57,6 +103,24 @@ sap.ui.define(
               },
             },
           });
+        },
+
+        _handleBookingSuccess: function (bookId, response) {
+          MessageBox.alert(
+            "Flight booked. Booking reference number is: " + bookId
+          );
+        },
+
+        _handleBookingError: function (oError) {
+          if (oError) {
+            if (oError.responseText) {
+              var oErrorMessage = JSON.parse(oError.responseText);
+              MessageBox.alert(
+                "Flight was not booked due to: " +
+                  oErrorMessage.error.message.value
+              );
+            }
+          }
         },
 
         _onBindingChange: function () {
